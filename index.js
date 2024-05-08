38813a6bfc0b84ab8f82013330fddb2385f763e7
Adrielle Moraes Gomes <adriellemoraesgomes@gmail.com>
2024-05-07 21:11:53 -0300

    feat: PetLife

---
+++ index.js
@@ -0,0 +1,9 @@
+var elementosDuvidas = document.querySelectorAll('.duvidas')
+
+elementosDuvidas.forEach(function (duvidas) {
+    duvidas.addEventListener('click', function () {
+        duvidas.classList.toggle('ativa')
+    })
+})
+
+
